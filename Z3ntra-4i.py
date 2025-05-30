093fd858f6c418b144c4c97fcced3fd2ba0dc506
JsmBlck <160460895+JsmBlck@users.noreply.github.com>
2025-05-30 19:39:28 +0800

    Update Z3ntra-4i.py

--- Z3ntra-4i.py
+++ Z3ntra-4i.py
@@ -141,7 +141,7 @@ app = FastAPI(lifespan=lifespan)
 async def healthcheck(request: Request):
     return {"status": "ok"}
 
-await simulate_analysis(chat_id, pair, expiry, full_name, username, user_id):
+await simulate_analysis(chat_id, pair, expiry, full_name, username, user_id)
     analysis_steps = [
         f"🤖 You selected {pair} ☑️\n\n⏳ Time: {expiry}\n\n🔎 Analyzing.",
         f"🤖 You selected {pair} ☑️\n\n⌛ Time: {expiry}\n\n🔎 Analyzing..",
